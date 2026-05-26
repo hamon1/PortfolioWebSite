f12519cd0bdb526abd5496bfa2c77eb96f2035c7
hamon1 <leejh021122@naver.com>
2026-05-26 23:29:01 +0900

    feat: pojects -> 팀 규모, 기한, 담당 구혁 데이터 추가

--- portfoliowebsite/src/components/project/ProjectCard.tsx
+++ portfoliowebsite/src/components/project/ProjectCard.tsx
@@ -7,20 +7,84 @@ type Props = {
 
 function ProjectCard({ project }: Props) {
     return (
-        <article className="project-card">
+        // <article className="project-card">
 
-            <h2>{project.title}</h2>
+        //     <h2>{project.title}</h2>
 
-            <p>{project.description}</p>
+        //     <p>{project.description}</p>
 
-            <div>
-                {project.tags.map(tag => (
-                    <span key={tag}>
-                        {tag}
-                    </span>
-                ))}
+        //     <div>   
+        //         <span> img </span>
+        //     </div>
+
+        //     <div>
+        //         {project.tags.map(tag => (
+        //             <span key={tag}>
+        //                 {tag}
+        //             </span>
+        //         ))}
+        //     </div>
+        // </article>
+        <section className="project-card featured">
+
+            <div className="project-thumbnail">
+                {/* <img src="./assets/travelbuddy.webp" alt="TravelBUDDY 썸네일"> */}
             </div>
-        </article>
+
+
+            <div className="project-content">
+
+
+                <div className="project-header">
+                    <h3 className="project-title">
+                        {project.title}
+                    </h3>
+
+                    <span className="project-badge">
+                        {project.featured}
+                    </span>
+                </div>
+
+
+                <p className="project-description">
+                    {project.description}
+                </p>
+
+                <p> {project.period} </p>
+
+                <p> {project.role} </p>
+
+                <p> {project.teamSize} </p>
+
+                <div>
+                    {project.tags.map(tag => (
+                        <span key={tag}>
+                            {tag} 
+                        </span>
+                    ))}
+                </div>
+
+
+                <div className="project-actions">
+                    <a
+                        href="https://github.com/example"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="project-link github"
+                    >
+                        GitHub
+                    </a>
+
+                    <a
+                        href="#"
+                        className="project-link demo"
+                    >
+                        Live Demo
+                    </a>
+                </div>
+
+            </div>
+        </section>
     );
 }
 
