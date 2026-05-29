36dcb599f42740538360d5b2cfb2956e6fe1244d
hamon1 <leejh021122@naver.com>
2026-05-30 00:10:50 +0900

    raw: raw(json) - feathertales 프로젝트 데이터(임시) 추가

--- portfoliowebsite/src/data/parsers/project.parser.ts
+++ portfoliowebsite/src/data/parsers/project.parser.ts
@@ -1,9 +1,11 @@
 import rawTravelBuddy from '../raw/travelbuddy.json';
 import rawFocat from '../raw/focat.json';
+import rawFeathertales from '../raw/feathertales.json';
 
 import { ProjectSchema } from '../schemas/project.schema';
 
 export const projects = [
     ProjectSchema.parse(rawTravelBuddy),
-    ProjectSchema.parse(rawFocat)
+    ProjectSchema.parse(rawFocat),
+    ProjectSchema.parse(rawFeathertales)
 ]
